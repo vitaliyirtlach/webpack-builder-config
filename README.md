672c01360416f6e0ba1fab10d0ab4ef631da79d2
vitaliyirtlach <w13vitaliy@gmail.com>
2021-03-10 21:34:09 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Webpack Builder Config for web frameworks
+
+## Supporting frameworks 
+- <h4>React</h4>
+
+
+### What can do this module?
+
+### Create an application
+```bash 
+    # nodejs basic setup
+    npm init -y 
+    # install this module
+    npm install webpack-builder-config
+    # run a module
+    build-config
+```
+
+### Our team 
+
+- vitaliyirtlach <https://github.com/vitaliyirtlach>
+- kirillirtlach   
---
+++ src/index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+const fs = require("fs")
+const inquirer = require("inquirer")
+const { join, dirname } = require("path")
+const {echo, exec} = require("shelljs")
+
+const runningPath = join(__dirname, "..")
+const version = exec('node --version', {silent:true})
+
+let root = __dirname
+if (runningPath.includes("node_modules")) {
+    root = runningPath.slice(0, runningPath.indexOf("\\node_modules"))
+}
+inquirer.prompt([
+    {
+        type: "list",
+        name: "framework-config",
+        message: "What framework's configuration would you like to get?",
+        choices: ["React"]
+    }, 
+    {
+        type: "input",
+        message: "Application name",
+        name: "app-name"
+    },
+    {
+        name: "description",
+        message: "Application description",
+        name: "app-description"
+    }
+])
+.then(answers => {
+    const framework = answers["framework-config"].toLowerCase()
+    const appName = answers["app-name"]
+    const appDescription = answers["app-description"]
+    const packageJSON = JSON.parse(fs.readFileSync(join(__dirname, `packages/${framework}.package.json`), 'utf8'))
+    const webpackConfig = fs.readFileSync(join(__dirname, `configs/${framework}.webpack.config.js`), 'utf8')
+    packageJSON.name = appName
+    packageJSON.description = appDescription
+    
+    fs.writeFileSync(join(root, "package.json"), JSON.stringify(packageJSON, null, "  "))
+    fs.writeFileSync(join(root, "webpack.config.js"), webpackConfig)
+    
+    echo('Installing a dependencies')
+    echo(`Node version: ${version}`)
+})
