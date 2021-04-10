35d6d0bcae86ba6139b19a767198c9029ddcbbda
vitaliyirtlach <w13vitaliy@gmail.com>
2021-04-10 19:18:16 +0300

    change .npmignore and add tests

--- src/__tests__/paths.spec.ts
+++ src/__tests__/paths.spec.ts
@@ -10,4 +10,13 @@ describe("Paths", () => {
     test("paths should have a Svelte", () => {
         expect("Svelte" in paths).toBe(true)
     })
+    test("paths should have a Riot", () => {
+        expect("RiotJS" in paths).toBe(true)
+    })
+    test("paths should have a Inferno", () => {
+        expect("InfernoJS" in paths).toBe(true)
+    })
+    test("paths should have a Polymer", () => {
+        expect("PolymerJS" in paths).toBe(true)
+    })
 })
